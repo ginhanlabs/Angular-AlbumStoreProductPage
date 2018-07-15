91ecc3c55a16afe63cdae6de4b2af11eda356364
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2018-07-15 14:23:14 -0400

    using productService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  _albumUrl: string = '../assets/album.json';
+  constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) =>{
+      response.json();
+  }
 }
