3d4e70746ea95af27bada27ebc432be08983fb51
ginhanlabsgit config --global user.name ginhanlabsgit config --global user.name ginhanlabs <ginhanlabs@gmail.com>
2018-07-20 00:07:06 -0400

    typo

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
-  private _productUrl: string = '../assets/products.json';
+  private _productsUrl: string = '../assets/products.json';
   
   constructor(private _http: Http) { }
 
@@ -20,7 +20,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productUrl)
+    return this._http.get(this._productsUrl)
     .map( response => <Product[]>response.json());
   }
 }
